3fb4a50ef4b86b1720837abf2489b342b53a39e6
Ayeetu <a.smoljan@hotmail.com>
2014-11-29 11:36:12 +0100

    Added header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Header</title>
+</head>
+<body>
+<header>
+	<nav>
+		<ul>
+			<li><a href="#">home</a></li>
+			<li><a href="#">home</a></li>
+			<li><a href="#">home</a></li>
+			<li><a href="#">home</a></li>
+		</ul>
+	</nav>
+</header>
+	
